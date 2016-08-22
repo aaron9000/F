8e97838f9079a7c76a794a93db3e3833fb89d4d3
Aaron Geisler <aaron.geisler.sloth@gmail.com>
2016-08-21 20:10:27 -0700

    commit all files

---
+++ Assets/F/Editor/FTests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace UnityTest
+{
+	[TestFixture]
+	[Category("F Tests")]
+	internal class FTests
+	{
+
+		[Test]
+		public void ShallowCloneTest()
+		{
+			var list = new List<int> (new int[] {1 ,2});
+			var clonedList = F.shallowClone<List<int>, int>(list);
+			list.Clear ();
+			Assert.AreEqual (list.Count, 0);
+			Assert.AreEqual (clonedList.Count, 2);
+			Assert.AreEqual (clonedList[0], 1);
+		}
+
+		[Test]
+		public void MapTest()
+		{
+			var list = new List<int> (new int[] {1 ,2});
+			var mappedList = F.map<int, string>(x => (x * x).ToString(), list);
+			Assert.AreEqual (mappedList[0], "1");
+			Assert.AreEqual (mappedList[1], "4");
+		}
+			
+		[Test]
+		public void FromPairsTest()
+		{
+			var pairs = new List<List<object>> ();
+			pairs.Add(new List<object>(new object[] {1 ,"2"}));
+			pairs.Add(new List<object>(new object[] {2 ,"3"}));
+
+			var dict = F.fromPairs<int, string>(pairs);
+			Assert.AreEqual (dict[1], "2");
+			Assert.AreEqual (dict[2], "3");
+		}
+
+		[Test]
+		public void ToPairsTest()
+		{
+			var dictionary = new Dictionary<string, int> ();
+			dictionary.Add ("a", 1);
+			dictionary.Add ("b", 2);
+
+			var list = F.toPairs<string, int>(dictionary);
+			Assert.AreEqual (list[0][0], "a");
+			Assert.AreEqual (list[0][1], 1);
+			Assert.AreEqual (list[1][0], "b");
+			Assert.AreEqual (list[1][1], 2);
+		}
+
+		[Test]
+		public void ReduceValueTest()
+		{
+			var list = new List<int> (new int[] {1 ,2});
+			var reducedValue = F.reduce<int, int> ((accum, value) => {
+				return accum + value;
+			}, 0, list);
+			Assert.AreEqual (reducedValue, 3);
+		}
+
+		[Test]
+		public void ReduceCollectionTest()
+		{
+			var list = new List<bool> (new bool[] {false, true, true});
+			var reducedCollection = F.reduce<List<int>, bool> ((accum, value) => {
+				accum.Add(value ? 1 : 0);
+				return accum;
+			}, new List<int>(), list);
+			Assert.AreEqual (reducedCollection.Count, 3);
+			Assert.AreEqual (reducedCollection[0], 0);
+			Assert.AreEqual (reducedCollection[1], 1);
+			Assert.AreEqual (reducedCollection[2], 1);
+		}
+	}
+}
---
+++ Assets/F/F.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+public static class F {
+
+	// TODO: can cache stuff that relied on reflection...
+	private static Dictionary<string, Dictionary<string, Type>> _cachedTypes = new Dictionary<string, Dictionary<string, Type>>();
+
+
+	//public static bool isEnumerable(Type t){
+	//	return (t.GetInterface ("IEnumerable") != null);
+	//}
+
+	public static bool isCollection(Type t){
+		return (t.GetInterface ("ICollection") != null);
+	}
+
+	public static bool isDictionary(Type t){
+		return (t.GetInterface ("IDictionary") != null);
+	}
+
+	public static T getValueForStructKey<T>(string key, object strct) {
+		return default(T);
+	}
+
+	public static T getValueForObjectKey<T>(string key, object obj) {
+		if (obj == null)
+			return default(T);
+		Type t = obj.GetType ();
+		PropertyInfo[] props = t.GetProperties ();
+		foreach (var info in props){
+			if (info.Name == key && info.GetType () == typeof(T) && info.CanRead) {
+				return (T)info.GetValue (obj, null);
+			}
+		}
+		return default(T);
+	}
+
+	public static T identity<T>(T value){
+		return value;
+	}
+
+	// TODO: want shallow clone of: collection, object, struct, dictionary
+	public static TCollection shallowClone<TCollection, TElement>(TCollection source) where TCollection: ICollection<TElement>, new(){
+		TCollection clone = new TCollection ();
+		// TODO: may need to do something different for dictionaries...
+		foreach(var value in source){
+			clone.Add (value);
+		}
+		return clone;
+	}
+
+	public static List<TOutput> map<TInput, TOutput>(Func<TInput, TOutput> mappingFunction, IEnumerable<TInput> collection){
+		List<TOutput> newList = new List<TOutput> ();
+		foreach(TInput value in collection){
+			newList.Add (mappingFunction (value));
+		}
+		return newList;
+	}
+
+	public static Dictionary<TKey, TValue> fromPairs<TKey, TValue>(List<List<object>> pairs){
+		Dictionary<TKey, TValue> newDict = new Dictionary<TKey, TValue> ();
+		foreach(List<object> pair in pairs){
+			newDict.Add ((TKey)pair [0], (TValue)pair [1]);
+		}
+		return newDict;
+	}
+
+	public static List<List<object>> toPairs<TKey, TValue>(Dictionary<TKey, TValue> dict){
+		List<List<object>> pairs = new List<List<object>>();
+		foreach(TKey key in dict.Keys){
+			pairs.Add (new List<object> (new object[] { key, dict [key] }));
+		}
+		return pairs;
+	}
+
+	public static TAccum reduce<TAccum, TElement> (Func<TAccum, TElement, TAccum> reducingFunction, TAccum startValue, IEnumerable<TElement> list) {
+		TAccum accum = startValue;
+		foreach(TElement value in list){
+			accum = reducingFunction (accum, value);
+		}
+		return accum;
+	}
+
+	public static List<TElement> filter<TElement>(Func<TElement, bool> filteringFunction,IEnumerable<TElement> collection){
+		return null;
+	}
+
+	public static Dictionary<TKey, TValue> zip<TKey, TValue>(IEnumerable<TKey> keys, IEnumerable<TValue> values){
+		return null;
+	}
+
+	public static List<TPluckedValue> pluck<TElement, TPluckedValue>(string key, IEnumerable<TElement> collection){
+		return null;
+	}
+
+
+
+}
